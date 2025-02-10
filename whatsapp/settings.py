46749be95b4cdd9648fbcf1fc0b900f26890fa29
wilsoft-gt <wilson.romero1992@gmail.com>
2025-02-10 17:51:38 -0600

    Updated calendar folder structure

--- whatsapp/settings.py
+++ whatsapp/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'whatsapp.wsgi.application'
 DATABASES = {
     'dev': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db copy.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }, 
     'production': {
         'ENGINE': 'django.db.backends.mysql',
